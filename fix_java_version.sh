fb9c1eb9b507dc77a1b582f8ea87b3f422a4fa79
Halaqi Dev <gasmmustafa11@gmail.com>
2026-08-26 18:35:17 +0300

    fix: update sed command

--- fix_java_version.sh
+++ fix_java_version.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-echo "Applying Java 17 fix..."
-    sed -i '' "s/VERSION_21/VERSION_17/g" android/capacitor-cordova-android-plugins/build.gradle
-    
+#!/usr/bin/env bash
+echo "Applying Java 17 fix to gradle files..."
+sed -i '' 's/VERSION_21/VERSION_17/g' android/capacitor-cordova-android-plugins/build.gradle
+sed -i '' 's/VERSION_21/VERSION_17/g' android/capacitor-android/build.gradle
+echo "Fix applied."
+
+
